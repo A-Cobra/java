d143da9e44ee20e575ed4ab40d843d075fc05e56
A-Cobra <conradbravina@gmail.com>
2023-06-16 17:17:56 -0500

    Added a User class and some dependencies

---
+++ jpa_crud/src/main/java/com/a_cobra/models/User.java
@@ -0,0 +1,30 @@
+package com.a_cobra.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@NamedQueries({
+        @NamedQuery(name = "User.findAll", query = "SELECT u From User u")
+})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String lastname;
+    private String phone;
+    private String email;
+    private String password;
+}
